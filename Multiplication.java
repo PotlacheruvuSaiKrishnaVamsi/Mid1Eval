3e26a9a1aaec946f5db7bef2add25f5a1bfc23fc
PotlacheruvuSaiKrishnaVamsi <Potlacheruvs.vamsi@st.niituniversity.in>
2019-09-25 11:25:21 +0530

    Changes made to function name in Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,5 +1,5 @@
 public class Multiplication implements Operation{
-	public String multiplication(double a, double b){
+	public String operation(double a, double b){
 		double mul = a * b;
 		return "Multiplication is "+mul;
 	}
