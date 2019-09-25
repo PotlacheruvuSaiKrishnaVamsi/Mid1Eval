dead8178adac93a065875567aee41e0c9d10cb37
Pradeep771 <43470198+Pradeep771@users.noreply.github.com>
2019-09-25 11:34:48 +0530

    capital S

--- Subtraction.java
+++ Subtraction.java
@@ -1,5 +1,5 @@
 public class Subtraction implements Operation{
-  public  string operation(double a, double b){
+  public String operation(double a, double b){
    double sub = a-b;
    return "Subtraction is "+sub;
 }
