e0c3c618283a4c04d189dae99c41d72efb613881
PotlacheruvuSaiKrishnaVamsi <Potlacheruvs.vamsi@st.niituniversity.in>
2019-09-25 11:24:47 +0530

    Changes made to Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,5 +1,5 @@
 public class Multiplication implements Operation{
-	public static String multiplication(double a, double b){
+	public String multiplication(double a, double b){
 		double mul = a * b;
 		return "Multiplication is "+mul;
 	}
