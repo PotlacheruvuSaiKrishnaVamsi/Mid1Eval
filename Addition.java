2b4e5df7abb3e97488c6d5e75c8772dd1680b27c
lokesh-venkata-sai <mamidil.sai@st.niituniversity.in>
2019-09-25 11:13:51 +0530

    removed static

--- Addition.java
+++ Addition.java
@@ -1,6 +1,6 @@
 public class Addition implements Operation{
-	public static String operation(double a, double b){
+	public String operation(double a, double b){
 		double sum=a+b;
-		return "Addition is"+sum;
+		return "Addition is "+sum;
 	}
 }
