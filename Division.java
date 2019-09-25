d7f9222d122691e434f9821c545a8551a6c2af10
PotlacheruvuSaiKrishnaVamsi <Potlacheruvs.vamsi@st.niituniversity.in>
2019-09-25 11:36:53 +0530

    Division.java file changed

--- Division.java
+++ Division.java
@@ -1,9 +1,10 @@
 public class Division implements Operation{
 	public String operation(double a, double b){
+		double div=0.00;
 		try{
-			double div = a / b;	
+			div = a / b;	
 		} catch(Exception e){
 		}
-		return "Division is "+div; 
+		return "Division is "+div; /*This statement returns the string*/
 	}
 }
